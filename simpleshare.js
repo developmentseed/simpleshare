a9572c217d0bbd19e82ea6198aa8b70eecbc2938
ptrl <per.porserot@gmail.com>
2010-10-14 16:39:46 +0200

    Drupal 7 release of drupal-6--1-0-beta4

--- simpleshare.js
+++ simpleshare.js
@@ -1,21 +1,25 @@
-Drupal.behaviors.simpleshare = function() {
-  $('a.simpleshare:not(.processed)').each(function() {
-    $(this).addClass('processed').click(function() {
-      // Cleanup any existing popups.
-      $('div.simpleshare-popup').remove();
-
-      // Make an AJAX call for this link.
-      var title = $(this).attr('title') ? $(this).attr('title') : $(document).attr('title');
-      var url = $(this).attr('href');
-      var ajax_url = Drupal.settings.basePath + 'simpleshare';
-      $.get(ajax_url, {'title': title, 'url': url}, function(data) {
-        $(document.body).append(data);
-        $('div.simpleshare-popup').show()
-        .children('span.close').click(function() {
+(function ($) {
+  Drupal.behaviors.simpleshare = {
+    attach: function() {
+      $('.simpleshare a:not(.processed)').each(function() {
+        $(this).addClass('processed').click(function() {
+          // Cleanup any existing popups.
           $('div.simpleshare-popup').remove();
+
+          // Make an AJAX call for this link.
+          var title = $(this).attr('title') ? $(this).attr('title') : $(document).attr('title');
+          var url = $(this).attr('href');
+          var ajax_url = Drupal.settings.basePath + 'simpleshare';
+          $.get(ajax_url, {'title': title, 'url': url}, function(data) {
+            $(document.body).append(data);
+            $('div.simpleshare-popup').show()
+            .children('span.close').click(function() {
+              $('div.simpleshare-popup').remove();
+            });
+          });
+          return false;
         });
       });
-      return false;
-    });
-  });
-};
+    }
+  };
+})(jQuery);
